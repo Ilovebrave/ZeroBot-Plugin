52a20f3a2eef08edb1047fdbaa207f867efff293
Ilovebrave <108995744+Ilovebrave@users.noreply.github.com>
2022-08-10 09:08:39 +0800

    ranme

--- main.go
+++ main.go
@@ -202,7 +202,7 @@ func init() {
 
 	// 通过代码写死的方式添加主人账号
 	// sus = append(sus, 3493798041)
-	// sus = append(sus, 87654321)
+	// sus = append(sus, 1685552317)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
